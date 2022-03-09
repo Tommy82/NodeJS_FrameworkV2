1ea92ee18a37854cc7b5acff00cff32ffe8d9456
Thomas Göttsching <support@thomasgoettsching.de>
2022-03-09 09:15:07 +0100

    Settings - Example

---
+++ resources/config/example/settings.js
@@ -0,0 +1,29 @@
+export const settings = {
+    webServer: {
+        port: 3000,
+        template: 'twig',
+        prefix: '',
+        sessionKey: 'abc'
+    }
+}
+
+export const database = {
+    // Interne Datenbank
+    default: {
+        type: 'mysql',
+        host: '',
+        port: 3306,
+        user: '',
+        pass: '',
+        database: '',
+    },
+    // Xentral Datenbank
+    xentral: {
+        type: 'mysql',
+        host: '',
+        port: 3306,
+        user: '',
+        pass: '',
+        database: '',
+    }
+}
