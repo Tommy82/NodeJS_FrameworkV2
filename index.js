e9eb0969e6ffee83dae14fb232b10a2158f5d6dc
Thomas Göttsching <support@thomasgoettsching.de>
2022-03-11 15:02:51 +0100

    Insert Custom Index

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ import './resources/backend/modules/_rights/_index.js';
 import './resources/backend/modules/_role/_index.js';
 //#endregion Import Modules
 
+// Import Custom Module
+import './index_custom.js';
+
 app.init(); // Initialisierung der App (inklusive Installation und Starten!)
 
